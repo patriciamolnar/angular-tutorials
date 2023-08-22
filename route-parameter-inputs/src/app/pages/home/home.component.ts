a125f92990009872223b01aa95e354673e68f666
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-08-22 11:27:45 +0100

    [route-parameter-inputs-singleton-app] - adding mini project code

---
+++ route-parameter-inputs/src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { nameData } from './home.static';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  @Input() name: string = '';
+  @Input() title: string = '';
+
+  names = nameData;
+
+  getNameData() {
+    return this.names.find(
+      (name) => name.value.toLowerCase() === this.name.toLowerCase()
+    );
+  }
+}
---
+++ route-parameter-inputs/src/app/pages/home/home.static.ts
@@ -0,0 +1,102 @@
+export const nameData = [  
+    {    
+        "value": "William",   
+         "meaning": "resolute protector",    
+         "gender": "M"  
+    },  
+    {    
+        "value": "James",   
+        "meaning": "supplanter",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Michael",    
+        "meaning": "who is like God",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "David",    
+        "meaning": "beloved",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Joseph",    
+        "meaning": "may he add",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Sophia",    
+        "meaning": "wisdom",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Emily",    
+        "meaning": "industrious, striving",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Olivia",   
+        "meaning": "olive tree",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Daniel",    
+        "meaning": "God is my judge",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Matthew",    
+        "meaning": "gift of God",    
+        "gender": "M" 
+    },  
+    {    
+        "value": "Emma",    
+        "meaning": "universal",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Liam",    
+        "meaning": "strong-willed warrior",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Ava",   
+        "meaning": "life",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Mia",    
+        "meaning": "mine or bitter",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Benjamin",    
+        "meaning": "son of the right hand",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Ethan",    
+        "meaning": "strong, firm",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Charlotte",    
+        "meaning": "free man",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Grace",    
+        "meaning": "divine favor",    
+        "gender": "F"  
+    },  
+    {    
+        "value": "Logan",    
+        "meaning": "little hollow",    
+        "gender": "M"  
+    },  
+    {    
+        "value": "Harper",    
+        "meaning": "harp player",    
+        "gender": "F"  
+    }
+]
