abb36b46d50c666acb3edcb3ad11e605411a0fdb
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-10-23 15:44:13 +0100

    [angular-recaptcha] - clearning up app.component html & ts files

--- angular-recaptcha/FE/src/app/app.component.ts
+++ angular-recaptcha/FE/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
@@ -9,13 +8,14 @@ import { ReCaptchaV3Service } from 'ng-recaptcha';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  result: any; 
+  
   constructor(
     private _http: HttpClient,
     private _recaptchaV3Srv: ReCaptchaV3Service
   ) {}
 
-  send(form: NgForm) {
-    if(form.invalid) return; 
+  send() {
     this._recaptchaV3Srv.execute('register_interest_form_submit').subscribe((token: string) => {
       const body = JSON.stringify({
         token: token, 
@@ -30,6 +30,7 @@ export class AppComponent {
 
       // Send token to backend
       this._http.post('http://localhost:3000/verify', body, httpOptions).subscribe((res: any) => {
+        this.result = res;
         console.log(res);
       });
     });
